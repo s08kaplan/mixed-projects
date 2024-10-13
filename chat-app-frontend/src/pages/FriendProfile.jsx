b7bda2bdfe3a2e650e6ef0045c4c23256b83a35e
Serkan <s8ersis@gmail.com>
2024-10-13 18:05:06 +0300

    add: friend profile

--- chat-app-frontend/src/pages/FriendProfile.jsx
+++ chat-app-frontend/src/pages/FriendProfile.jsx
@@ -3,13 +3,16 @@ import useAuthCalls from '../custom-hooks/useAuthCalls'
 import { useParams } from 'react-router-dom'
 import useUsers from '../custom-hooks/useUsers'
 import { useSelector } from 'react-redux'
+import { VscSend } from "react-icons/vsc";
 
 const FriendProfile = () => {
     const { friendId } = useParams()
     const { getUsers } = useUsers()
     const { userDetail } = useSelector(state => state.users)
+    const { user } = useSelector(state => state.auth)
     const [userInfo, setUserInfo] = useState({})
-
+const isFriend = user?.friends.find(user =>  userDetail?.friends.includes(user))
+console.log(isFriend);
     console.log(userDetail);
 console.log("friend id: ", friendId);
     useEffect(() => {
@@ -18,8 +21,22 @@ console.log("friend id: ", friendId);
     
   return (
     <section>
-        <article>
-            <img src={userDetail?.image} alt="" />
+        <article className='flex flex-col justify-center items-center gap-2 p-2'>
+            <img src={userDetail?.image || "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_640.png"} alt="user image" width={250} className='rounded-full'/>
+            <span>{userDetail?.username}</span>
+        </article>
+        <article className='flex justify-center'>
+            {isFriend ?  <div className="mt-2 flex items-center gap-2 ">
+                <button className=' flex justify-center items-center border border-white-800 w-12 h-8 hover:bg-indigo-800 hover:border-lime-500 transition-colors duration-200'><VscSend className="text-2xl hover:text-sky-500 transition-colors duration-200" /></button>
+                <input
+                  id="message"
+                  name="message"
+                  type="text"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+                
+              </div> : <button className="flex w-[60%] justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Send friend request</button>}
         </article>
     </section>
   )
--- chat-app-frontend/src/pages/NotFound.jsx
+++ chat-app-frontend/src/pages/NotFound.jsx
@@ -7,9 +7,24 @@ const NotFound = () => {
        navigate("/")
      }
   return (
-    <section className='h-dvh bg-[url("https://cdn.pixabay.com/photo/2016/12/14/23/08/page-not-found-1907792_640.jpg")] bg-cover bg-no-repeat flex justify-center items-center'>
-       <button className=' text-secondary border-2 border-violet-700 p-2 rounded-lg' onClick={handleNavigate}>Go Home</button>
-    </section>
+    <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+        <div className="text-center">
+          <p className="text-base font-semibold text-indigo-600">404</p>
+          <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">Page not found</h1>
+          <p className="mt-6 text-base leading-7 text-gray-600">Sorry, we couldn’t find the page you’re looking for.</p>
+          <div className="mt-10 flex items-center justify-center gap-x-6">
+            <a
+              href="#"
+              className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Go back home
+            </a>
+            <a href="#" className="text-sm font-semibold text-gray-900">
+              Contact support <span aria-hidden="true">&rarr;</span>
+            </a>
+          </div>
+        </div>
+      </main>
   )
 }
 
